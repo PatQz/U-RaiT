1c4071f71f9f32533af9e84dd1bb922b9f7bc22a
PatQz <patqrz@gmail.com>
2016-11-09 13:00:27 -0700

    Initial commit.

---
+++ app/src/main/java/com/example/android/u_rait/UserInformation.java
@@ -0,0 +1,20 @@
+package com.example.android.u_rait;
+
+/**
+ * Created by Pat on 09/11/2016.
+ */
+
+public class UserInformation {
+
+    public String name;
+    public String address;
+
+    public UserInformation(){
+
+    }
+
+    public UserInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
