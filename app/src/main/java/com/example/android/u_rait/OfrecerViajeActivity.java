f1aca01c7146357f01c89421c7990ccbe8476d01
PatQz <patqrz@gmail.com>
2016-11-15 09:05:03 -0700

    Actividad ofrecer viaje.

--- app/src/main/java/com/example/android/u_rait/OfrecerViajeActivity.java
+++ app/src/main/java/com/example/android/u_rait/OfrecerViajeActivity.java
@@ -11,6 +11,9 @@ import java.util.Calendar;
 
 public class OfrecerViajeActivity extends AppCompatActivity implements View.OnClickListener{
     private Button tiempo;
+    Calendar mcurrentTime = Calendar.getInstance();
+    int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+    int minute = mcurrentTime.get(Calendar.MINUTE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +21,17 @@ public class OfrecerViajeActivity extends AppCompatActivity implements View.OnCl
         setContentView(R.layout.activity_ofrecer_viaje);
 
         tiempo = (Button) findViewById(R.id.textViewTime);
-
+        setTiempo(hour, minute);
         tiempo.setOnClickListener(this);
+    }
+
+    private void setTiempo(int selectedHour,int selectedMinute){
+        String hourScheledule = "AM";
+        if(selectedHour >12){
+            selectedHour= selectedHour %12;
+            hourScheledule = "PM";
+        }
+        tiempo.setText( selectedHour + ":" + selectedMinute + " " + hourScheledule);
     }
 
     @Override
@@ -30,14 +42,11 @@ public class OfrecerViajeActivity extends AppCompatActivity implements View.OnCl
     }
 
     private void setTiempo(){
-        Calendar mcurrentTime = Calendar.getInstance();
-        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-        int minute = mcurrentTime.get(Calendar.MINUTE);
         TimePickerDialog mTimePicker;
         mTimePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                tiempo.setText( selectedHour + ":" + selectedMinute);
+                setTiempo(selectedHour,selectedMinute);
             }
         }, hour, minute, false);//No 24 hour time
         mTimePicker.setTitle("Select Time");
